52e2d886710d5553c9c1fd8132847d77b20d231a
Gowshik-S <saairamgowshik@gmail.com>
2026-01-01 21:22:50 +0530

    v2.4.1

--- backend/settings.py
+++ backend/settings.py
@@ -8,6 +8,9 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
+# CSRF Trusted Origins
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost:8000,http://127.0.0.1:8000', cast=Csv())
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
